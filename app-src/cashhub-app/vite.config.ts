8018238ab2ac7ab7351ced507b9207f32c8f98c9
NorbertDerHorvath <horvath-norbert@web.de>
2026-02-25 21:48:02 +0100

    Update vite.config.ts

--- app-src/cashhub-app/vite.config.ts
+++ app-src/cashhub-app/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  // GitHub Pages almappa
   base: "/apps/cashhub-app/",
   plugins: [react()],
   build: {
